40857508a99e5698d42238a3a12b0f4c069b3048
Razumovskaya-ins-19-1 <71125145+Razumovskaya-ins-19-1@users.noreply.github.com>
2020-10-20 14:32:53 +0300

    Лабораторная работа №7

---
+++ Лабораторная работа №7/Point.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Лабораторная_работа__8
+{
+    class Point 
+    {
+        private double x1, y1, x2, y2;
+        private Point() { }
+
+
+        public Point(double px1, double py1, double px2, double py2)
+        {
+             
+            x1 = px1;
+            y1 = py1;
+            x2 = px2;
+            y2 = py2;
+        }
+        public void Load()
+        {
+            x1 = Convert.ToDouble(Console.ReadLine());
+            y1 = Convert.ToDouble(Console.ReadLine());
+            x2 = Convert.ToDouble(Console.ReadLine());
+            y2 = Convert.ToDouble(Console.ReadLine());
+        }
+        public static Point File()
+        { 
+            double x11= Convert.ToDouble(Console.ReadLine());
+            double y11 = Convert.ToDouble(Console.ReadLine());
+            double x22 = Convert.ToDouble(Console.ReadLine());
+            double y22 = Convert.ToDouble(Console.ReadLine());
+            return new Point(x11, y11, x22, y22);
+        }
+        public double distance2()
+        {
+            return Math.Sqrt(x1 * x1 + y1 * y1); 
+        }
+        public double distance1()
+        {
+            return Math.Sqrt(Math.Pow((x2-x1),2) + Math.Pow((y2-y1),2));
+        }
+        public void Info()
+        {
+            Console.WriteLine(string.Format(" Первая точка X1={0:0.000}, Y1={1:0.000}", x1, y1));
+            Console.WriteLine(string.Format(" Вторая точка X2={0:0.000}, Y2={1:0.000}", x2, y2));
+            Console.WriteLine(string.Format(" Расстояние от начала координат до первой точки: S={0:0.000}", distance2()));
+            Console.WriteLine(string.Format(" Расстояние  между точками: d={0:0.000}", distance1()));
+        }
+       public void Info(ConsoleColor fg, ConsoleColor bg)
+        {
+            Console.ForegroundColor = fg;
+            Console.BackgroundColor = bg;
+            Console.Clear();
+            Info();
+        }
+    }
+}
---
+++ Лабораторная работа №7/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO; 
+
+namespace Лабораторная_работа__8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+#if !DEBUG
+                TextWriter save_out = Console.Out;
+                TextReader save_in = Console.In;
+                var new_out = new StreamWriter(@"Out.txt");
+                var new_in = new StreamReader(@"In.txt");
+                Console.SetOut(new_out);
+                Console.SetIn(new_in);
+#endif
+            Point p1, p2;
+#if DEBUG
+            p2 = new Point(2, 6, 5, 4);
+            p2.Info(ConsoleColor.White, ConsoleColor.DarkMagenta);
+#endif
+#if !DEBUG
+                p1=Point.File();
+                p1.Info();
+                p2= new Point(8, 5, 6, 9);
+                p2.Info();
+#endif
+#if !DEBUG
+                Console.SetOut(save_out); new_out.Close();
+                Console.SetIn(save_in); new_in.Close();
+#else
+            Console.ReadKey();
+#endif
+            
+        }
+    }
+}
