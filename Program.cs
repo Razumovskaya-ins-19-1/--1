e69baaa0c65c93efc0706da009a49c2f6bb349eb
Razumovskaya-ins-19-1 <71125145+Razumovskaya-ins-19-1@users.noreply.github.com>
2020-10-20 12:23:58 +0300

    Лабораторная работа №6(2)

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;  
+
+namespace Лабораторная_работа_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if !DEBUG
+            TextWriter save_out = Console.Out;
+            TextReader save_in = Console.In;
+            var new_out = new StreamWriter(@"Out.txt");
+            var new_in = new StreamReader(@"In.txt");
+            Console.SetOut(new_out);
+            Console.SetIn(new_in);
+#endif
+            Cylinder C = new Cylinder();
+            C.Load();
+            C.Info();
+#if !DEBUG
+            Console.SetOut(save_out); new_out.Close();
+            Console.SetIn(save_in); new_in.Close();
+#else
+            Console.ReadKey();
+#endif
+        }
+    }
+}
---
+++ Лабораторная работа №6(2)/Cylinder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Лабораторная_работа_6
+{
+    class Cylinder
+    {
+        private double r, h;
+        private const double pi = 3.14;
+
+        public double Area()
+        {
+            return 2 * pi * r * (r + h);
+        }
+        public double Volume()
+        {
+            return pi * Math.Pow(r, 2) * h;
+        }
+        public void Info()
+        {
+            String str = "     **********     \n" +
+                         "    *          *    \n" +
+                         "    **        **    \n" +
+                         "    * ******** *    \n" +
+                         "    *          *    \n" +
+                         "    *          *    \n" +
+                         "    *          *    \n" +
+                         "    *          *    \n" +
+                         "    *          *    \n" +
+                         "     **********    \n";
+            Console.WriteLine(str);
+            Console.WriteLine(string.Format("R={0:0.000}, H={1:0.000}", r, h));
+            Console.WriteLine(string.Format("S={0:0.000}", Area()));
+            Console.WriteLine(string.Format("V={0:0.000}", Volume()));
+        }
+        public void Load()
+        {
+            r = Convert.ToDouble(Console.ReadLine());
+            h = Convert.ToDouble(Console.ReadLine());
+        }
+    }
+}
