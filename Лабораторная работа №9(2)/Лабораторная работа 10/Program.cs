1a57220cc510724f6de6afda04871d778ac7a0bd
Razumovskaya-ins-19-1 <71125145+Razumovskaya-ins-19-1@users.noreply.github.com>
2020-12-16 19:24:09 +0300

    Лабораторная работа №9

---
+++ Лабораторная работа №9(2)/Лабораторная работа 10/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Лабораторная_работа_10
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.BackgroundColor = ConsoleColor.Cyan;
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.Clear();
+			Console.Title = "Лабораторная работа №9";
+			Suspitions p1 = new Suspitions("Бенхард Джонатан Вегард", "Наследство", 3);
+			p1.UlPlus(4);
+			p1.UlMinus(2);
+			p1.DrowEmblema();
+			Console.WriteLine("\n \n \n \n \n ");
+			Incendent k1 = new Incendent("Бруклинский мост", DateTime.Today, 25);
+			k1.UlPlus(5);
+			k1.UlMinus(2);
+			k1.DrowEmblema();
+			Console.WriteLine("\n \n \n \n \n ");
+			Console.ReadKey(); 
+		}
+	}
+}
---
+++ Лабораторная работа №9(2)/Лабораторная работа 10/Suspitions.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Лабораторная_работа_10
+{
+    class Suspitions: IDetect, IVisual
+    {
+        private string FIO, Motiv;
+        private int Kolul;
+        public Suspitions(string pFIO, string pMotiv, int pKolul)
+        {
+            FIO = pFIO;
+            Motiv = pMotiv;
+            Kolul = pKolul;
+        }
+        public void UlPlus(int pUl)
+        {
+            Kolul += pUl;
+        }
+        public void UlMinus (int pUl)
+        {
+            Kolul += pUl;
+        }
+        public string Name
+        {
+            get
+            {
+                return FIO + "\n Мотив: " + Motiv + "\n Количество улик: " + Kolul.ToString();
+            }
+            set
+            {
+                FIO = value;
+            }
+        }
+        public void DrowEmblema()
+        {
+            Console.WriteLine
+                (
+                "******  *    \n" +
+                "*       *    \n" +
+                "*       *    \n" +
+                "******  *****\n" +
+                "     *  *   *\n" +
+                "     *  *   *\n" +
+                "******  *   *\n"
+                );
+            Console.WriteLine(Name);
+        }
+    }
+    class Incendent : IDetect, IVisual
+    {
+        string Place;
+        DateTime Time;
+        int KolWitness;
+        public Incendent (string pPlace, DateTime pTime, int pKolWitness )
+        {
+            Place = pPlace;
+            Time = pTime;
+            KolWitness = pKolWitness;
+        }
+        public void UlPlus(int pUl)
+        {
+            KolWitness += pUl;
+        }
+        public void UlMinus(int pUl)
+        {
+            KolWitness += pUl;
+        }
+        public string Name
+        {
+            get
+            {
+                return "Место проишествия: " + Place + "\n Время: " + Time + "\n Количество свидетелей: " + KolWitness.ToString();
+            }
+            set
+            {
+                Place = value;
+            }
+        }
+        public void DrowEmblema()
+        {
+            Console.WriteLine
+                (
+                "******  *    \n" +
+                "*       *    \n" +
+                "*       *    \n" +
+                "******  *****\n" +
+                "*       *   *\n" +
+                "*       *   *\n" +
+                "******  *   *\n"
+                );
+            Console.WriteLine(Name);
+        }
+    }
+
+    public interface IDetect
+    {
+        void UlPlus(int pUl);
+        void UlMinus(int pUl);
+    }
+    public interface IVisual
+    {
+        string Name { get; set; }
+        void DrowEmblema();
+    }
+
+}
+
