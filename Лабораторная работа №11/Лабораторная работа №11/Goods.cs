511c8a45f40f2135c58dd50a0ef7bcc4083ef3cb
Razumovskaya-ins-19-1 <71125145+Razumovskaya-ins-19-1@users.noreply.github.com>
2020-12-19 15:32:21 +0300

    Лабораторная работа №11

---
+++ Лабораторная работа №11/Лабораторная работа №11/Goods.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Лабораторная_работа__11
+{
+    enum category
+    {
+        A,
+        B,
+        C,
+        D,
+        Категории_нет
+    }
+    class Goods
+    {
+        string ID { get; set; }
+        string Name { get; set; }
+        public category categ { get; set; }
+        public int kol { get; set; }
+        public float price { get; set; }
+        public Boolean prosr { get; set; }
+        public Boolean skid { get; set; }
+        public static Goods Creat(string str)
+        {
+            Goods g = new Goods();
+            if (str.EndsWith(","))
+                str += "false";
+            while (str.IndexOf(",,") > 0)
+            {
+                str = str.Insert((str.IndexOf(",,") + 1), "false");
+            }
+              string[] el = str.Split(',');
+               if (el.Length > 7)
+               {
+                for (int t = el.Length - 7; t != 0; t--)
+                {
+                    el[1] += ", " + el[2];
+                    for (int i = 3; i < el.Length; i++)
+                        el[i - 1] = el[i];
+                }
+               }
+               g.ID = el[0].Trim(); 
+               g.Name = el[1].Trim();
+               string tmp = el[2].Trim();
+               switch (tmp)
+               {
+                   case "A": { g.categ = category.A; break; };
+                   case "B": { g.categ = category.B; break; };
+                   case "C": { g.categ = category.C; break; };
+                   case "D": { g.categ = category.D; break; };
+                   default: { g.categ = category.Категории_нет; break; };
+               }
+               g.kol = Convert.ToInt32(el[3].Trim());
+               g.price = Convert.ToSingle(el[4].TrimStart('$').Replace('.', ','));
+               g.prosr = Convert.ToBoolean(el[5]);
+               g.skid = Convert.ToBoolean(el[6]);
+               return g;
+           }
+
+           public override string ToString()
+           {
+               string s = string.Format(
+                   "*******************************************************\n" +
+                   "ID: {0}, Наименование: {1}, Категория: {2}, \n" +
+                   "Количество: {3}, Цена: ${4},\n" +
+                   "Признак просроченности: {5}, Наличие скидки: {6}",
+                   ID, Name, categToStr(categ), kol, price, prosrToStr(prosr), skidToStr(skid));
+               return s;
+           }
+           private static string categToStr(category tmp)
+           {
+               switch (tmp)
+               {
+                   case category.A: return "A";
+                   case category.B: return "B";
+                   case category.C: return "C";
+                   case category.D: return "D";
+                   default: return "Категории нет";
+               }
+           }
+           private static string prosrToStr(Boolean h)
+           {
+               if (h == true) return "есть";
+               else return "нет";
+           }
+           private static string skidToStr(Boolean h)
+           {
+               if (h == true) return "есть";
+               else return "нет";
+           } 
+       }
+        }
+   
---
+++ Лабораторная работа №11/Лабораторная работа №11/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+namespace Лабораторная_работа__11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader potok = new StreamReader("lr11_24.csv");
+#if !DEBUG
+            TextWriter save_out = Console.Out;
+            var new_out = new StreamWriter(@"out.txt");
+            Console.SetOut(new_out);
+#endif
+            List<Goods> all = new List<Goods>();
+            try
+            {
+                string line = potok.ReadLine();
+                while ((line = potok.ReadLine()) != null)
+                {
+                    all.Add(Goods.Creat(line));
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("******Задача1*******");
+            float Max_prosr = (from p in all
+                               where p.prosr == true
+                               select p.price).Max();
+            Goods rich_goods1 = (from p in all
+                                where (p.prosr == true) && (p.price == Max_prosr)
+                                select p).First();
+            Console.WriteLine(rich_goods1);
+
+            float Max_no_prosr = (from p in all
+                               where p.prosr == false
+                               select p.price).Max();
+            Goods rich_goods2 = (from p in all
+                                where (p.prosr == false) && (p.price == Max_no_prosr)
+                                select p).First();
+            Console.WriteLine(rich_goods2);
+
+            Console.WriteLine("******Задача2*******");
+            int outcateg = all.FindAll(p => p.categ == category.Категории_нет).ToList().Count;
+            Console.WriteLine("Количество товара без категории: {0}", outcateg);
+
+            Console.WriteLine("******Задача3*******");
+            float A = (from p in all
+                                  where (p.categ == category.A) && (p.skid == true)
+                                  select p.price).Max();
+            Goods goodsA = (from p in all
+                                 where (p.categ == category.A) && (p.price == A) && (p.skid == true)
+                            select p).First();
+            Console.WriteLine("Самый дорогой товар в категории А: \n  {0}", goodsA);
+
+            float B = (from p in all
+                       where (p.categ == category.B) && (p.skid == true)
+                       select p.price).Max();
+            Goods goodsB = (from p in all
+                            where (p.categ == category.B) && (p.price == B) && (p.skid == true)
+                            select p).First();
+            Console.WriteLine("Самый дорогой товар в категории B: \n {0}", goodsB);
+
+            float C = (from p in all
+                       where (p.categ == category.C) && (p.skid == true)
+                       select p.price).Max();
+            Goods goodsC = (from p in all
+                            where (p.categ == category.C) && (p.price == C) && (p.skid == true)
+                            select p).First();
+            Console.WriteLine("Самый дорогой товар в категории C: \n {0}", goodsC);
+
+            float D = (from p in all
+                       where (p.categ == category.D) && (p.skid == true)
+                       select p.price).Max();
+            Goods goodsD = (from p in all
+                            where (p.categ == category.D) && (p.price == D) && (p.skid == true)
+                            select p).First();
+            Console.WriteLine("Самый дорогой товар в категории D: \n {0}", goodsD);
+
+            Console.WriteLine("******Задача4*******");
+            float summa = (from p in all
+                           where (p.skid == true)
+                           select (p.price) * (p.kol)).Sum();
+            Console.WriteLine("Объем товаров в валюте с учётом скидки {0}", summa);
+#if !DEBUG
+            Console.SetOut(save_out);
+            new_out.Close();
+#endif 
+        }
+    }
+}
