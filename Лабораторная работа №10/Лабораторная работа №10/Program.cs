550547a87f02768dd2b7aee3b8d839f777f6b1f3
Razumovskaya-ins-19-1 <71125145+Razumovskaya-ins-19-1@users.noreply.github.com>
2020-12-19 15:26:21 +0300

    Лабораторная работа №10

---
+++ Лабораторная работа №10/Лабораторная работа №10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Лабораторная_работа__10
+{
+  class Program
+   {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+
+            float c1, c2;
+            Библиотека mtr1 = new Библиотека();
+            mtr1.Generator(2, 3);
+            mtr1.Save(@"Matrix1.txt");
+            c1 = mtr1.Sum("Matrix1.txt");
+
+            Библиотека mtr2 = new Библиотека();
+            mtr2.Generator(3, 2);
+            mtr2.Save(@"Matrix2.txt");
+            c2 = mtr2.Sum("Matrix2.txt");
+
+            Console.WriteLine("Сумма элементов с четной суммой индексов первой матрицы равна {0}", c1);
+            Console.WriteLine("Сумма элементов с четной суммой индексов второй матрицы равна {0}", c2);
+            Console.WriteLine("Сумма элементов с четной суммой индексов двух матриц {0}", (c1 + c2));
+        }
+    }
+}
