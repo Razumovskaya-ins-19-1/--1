b27a12d834d2acee8a752b84e3a91f620830f706
Razumovskaya-ins-19-1 <71125145+Razumovskaya-ins-19-1@users.noreply.github.com>
2021-04-27 12:59:00 +0300

    Лабораторная работа №19

---
+++ Лабораторная работа №19(1)/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Лабораторная_работа__19_1_
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int N = 10;
+            try
+            {
+                N = Convert.ToInt32(FC.Text);
+            }
+            catch (Exception ex)
+            {
+                this.Title = "Только целое число!!!";
+            }
+            GenerateShapes(N);
+        }
+        private void GenerateShapes(int N)
+        {
+            Random rndShapeType = new Random(DateTime.Now.Millisecond);
+            Random rndStyle = new Random(DateTime.Now.Second);
+            Random rndPosition = new Random(DateTime.Now.Minute);
+            Random rndSize = new Random(DateTime.Now.Minute);
+
+            for (int i=0; i<N; i++)
+            {
+                Shape currentShape;
+                int shapeType = rndShapeType.Next(0, 3);
+                switch (shapeType)
+                {
+                    case 0: currentShape = new Ellipse(); break;
+                    default: currentShape = new Rectangle(); break;
+                }
+                    
+                int shapeStyle = rndStyle.Next(0, 4)+1; 
+                string styleName = "style" + shapeStyle.ToString();
+                Style currentStyle = (Style)this.FindResource(styleName);
+                currentShape.Style = currentStyle;
+                currentShape.Width = rndSize.Next(10, 200);
+                currentShape.Height = rndSize.Next(10, 100);
+                MainCanvas.Children.Add(currentShape);
+                Canvas.SetLeft(currentShape, rndPosition.Next(5, 750));
+                Canvas.SetTop(currentShape, rndPosition.Next(5, 370));
+
+            }
+        }
+    }
+}
