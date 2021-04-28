c27c3f4d02bf27944c6606cb95d0e430c446577a
Razumovskaya-ins-19-1 <71125145+Razumovskaya-ins-19-1@users.noreply.github.com>
2021-04-28 17:15:31 +0300

    Лабораторная работа №21

---
+++ Лабораторная работа №21/DataTier/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DataTier
+{          
+    public class Поездка
+    {   
+        public string Транспорт { get; set; }
+        public string Пункт_отправки { get; set; }
+        public string Пункт_назначения { get; set; }
+        public float Стоимость { get; set; }
+        public static Поездка Create(string str)
+        {
+            Поездка trip = new Поездка();
+            string[] el = str.Split('*');
+            trip.Транспорт = el[0].Trim();
+            trip.Пункт_отправки = el[1].Trim();
+            trip.Пункт_назначения = el[2].Trim();
+            el[3] = el[3].Replace('.', ',');
+            trip.Стоимость = Convert.ToSingle(el[3]);
+            return trip; 
+        }
+    }
+
+    public class Все_поездки
+    {
+
+        public static List<Поездка> Получить_все_поездки(StreamReader txr)
+        {
+            //TextReader save_in = Console.In;
+            //StreamReader txr = new StreamReader(@"data.txt");
+            //Console.SetIn(txr);
+            List<Поездка> list = new List<Поездка>();
+            string line;
+                while ((line = txr.ReadLine()) != null)
+                {
+                    list.Add(Поездка.Create(line));
+                }
+                return list;
+
+         //Console.SetIn(save_in);
+         //   txr.Close();
+        }
+
+        public static void Сохранить_товары(List<Поездка> поездки)
+        {
+
+        }
+   
+    }
+
+}
---
+++ Лабораторная работа №21/LogicTier/Class1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataTier;
+using System.IO;
+
+namespace LogicTier
+{
+    public class ТурПоездки
+    {
+        private Поездка _поездка;
+        public ТурПоездки(Поездка p)
+        {
+            _поездка = p;
+        }
+        public string ТранспортПоездки
+        {
+            get { return _поездка.Транспорт; }
+            set { _поездка.Транспорт = value; }
+        }
+        public string ПунктОтправкиПоездки
+        {
+            get { return _поездка.Пункт_отправки; }
+            set { _поездка.Пункт_отправки = value; }
+        }
+        public string ПунктНазначенияПоездки
+        {
+            get { return _поездка.Пункт_назначения; }
+            set { _поездка.Пункт_назначения = value; }
+        }
+        public float СтоимостьПоездки
+        {
+            get { return _поездка.Стоимость; }
+            set { _поездка.Стоимость = value; }
+        }
+
+
+        public String ПредставлениеПоездки
+        {
+            get { return _поездка.Транспорт + " : " + _поездка.Пункт_отправки + " : " + _поездка.Пункт_назначения + " : " + _поездка.Стоимость.ToString("C"); }
+        }
+    }
+    public class Тур
+    {
+        StreamReader txr = new StreamReader(@"data.txt");
+        private List<ТурПоездки> _поездки = new List<ТурПоездки>();
+        int s = 0;
+        float ss, r = 0;
+        public Тур()
+        {
+
+            List<Поездка> tmp = Все_поездки.Получить_все_поездки(txr);
+            foreach (var t in tmp)
+            {
+                _поездки.Add(new ТурПоездки(t));
+                if (t.Транспорт == "Автобус")
+                {
+                    s += 1;
+                }
+
+            }
+
+             ss = (from p in tmp
+                        where p.Транспорт == "Самолёт"
+                        select p.Стоимость).Sum();
+            r = (from p in tmp
+                  where p.Транспорт == "Самолёт"
+                  select p.Стоимость).Max();
+
+        }
+        public List<ТурПоездки> СписокПоездок
+        {
+            get { return _поездки; }
+        }
+        public string НаименованиеТурАгенство
+        {
+            get { return "Ласточка"; }
+        }
+        public int КоличествоАвтобусныхРейсов
+        {
+            get { return s; }
+        }
+        public float СуммарнаяСтоимостьбилетовНаСамолёт
+        {
+            get { return ss;  }
+        }
+        public float СамыйДорогойбилет
+        {
+            get { return r; }
+        }
+    }
+}
