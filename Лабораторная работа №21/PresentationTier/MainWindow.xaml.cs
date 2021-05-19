b95c8c165db07db58990cf79a05c45ad5ddc5b2f
Razumovskaya-ins-19-1 <71125145+Razumovskaya-ins-19-1@users.noreply.github.com>
2021-05-19 13:10:22 +0300

    Лабораторная работа №21

---
+++ Лабораторная работа №21/PresentationTier/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.IO;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LogicTier;
+
+namespace PresentationTier
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            Тур logicTire = new Тур();
+            this.DataContext = logicTire;
+            InitializeComponent();
+        }
+    }
+}
